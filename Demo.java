c3ce755da6c396a63499b95e45877f26fda0f176
kaviya2502 <kaviyarajakumarkaviyarajakumar@gmail.com>
2025-03-12 15:11:38 +0530

    i am kaviya

---
+++ Demo.java
@@ -0,0 +1,8 @@
+public class Demo{
+    public static void main(String args[]){
+        int numOne=10;
+        System.out.println(numOne);
+        }
+    }
+
+
---
+++ Method/Method1.java
@@ -0,0 +1,10 @@
+public class Method1{
+    public void add(int num1,int num2){
+        int num3=num1+num2;
+        System.out.println(num3);
+   }
+public static void main(String args[]){
+    Method1 obj=new Method1();
+    obj.add(10,2);
+}
+    }
