89bc0f5867327d28e396a229f025b990169be8a7
kaviya2502 <kaviyarajakumarkaviyarajakumar@gmail.com>
2025-03-12 15:46:05 +0530

    return value

---
+++ Return/Add.java
@@ -0,0 +1,10 @@
+public class Add{
+    public int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String args[]){
+        Add obj=new Add();
+        int c=obj.add(10,20);
+        System.out.println(c);
+        }
+}
