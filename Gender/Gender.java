8dd3220035f736874d6fa2b587c140fe068f7450
kaviya2502 <kaviyarajakumarkaviyarajakumar@gmail.com>
2025-03-13 11:44:23 +0530

    gender java

---
+++ Gender/Gender.java
@@ -0,0 +1,12 @@
+public class Gender{
+    public static void main(String args[]){
+        char gender='M';
+        if(gender=='M')
+        {
+            System.out.println("Male");
+        }
+        else{
+            System.out.println("Female");
+        }
+    }
+        }
