d368be1461470765ecad7fa5728d89074e6bd4b7
kaviya2502 <kaviyarajakumarkaviyarajakumar@gmail.com>
2025-03-13 11:35:41 +0530

    positivenegative finder

---
+++ PositiveNegativeFinder/Positive.java
@@ -0,0 +1,12 @@
+public class Positive{
+    public static void main(String args[]){
+        int num=27;
+        if(num>=0)
+        {
+            System.out.println("Positive");
+        }
+        else{
+            System.out.println("Negative");
+        }
+        }
+    }
