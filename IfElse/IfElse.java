3134f57986d7ba12854caa5efb9c4c3a845404e2
kaviya2502 <kaviyarajakumarkaviyarajakumar@gmail.com>
2025-03-13 11:01:14 +0530

    IfElse java'

---
+++ IfElse/IfElse.java
@@ -0,0 +1,10 @@
+public class IfElse{
+    public static void main(String args[]){
+        if(true){
+            System.out.println("Correct");
+        }
+    else{
+        System.out.println("Wrong");
+    }
+    }
+    }
