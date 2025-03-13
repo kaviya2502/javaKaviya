6ab14600c8d5e74d86e298718535d0894157f868
kaviya2502 <kaviyarajakumarkaviyarajakumar@gmail.com>
2025-03-13 10:35:23 +0530

    arithmetic operator java

---
+++ ArithmeticOperator/Arithmetic.java
@@ -0,0 +1,10 @@
+public class Arithmetic{
+    public static void main(String args[]){
+    Arithmetic Operator;
+    System.out.println(10+2);
+    System.out.println(10-2);
+    System.out.println(10*2);
+    System.out.println(10/2);
+    System.out.println(10%2);
+}
+}
