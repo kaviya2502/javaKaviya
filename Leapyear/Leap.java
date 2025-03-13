34a319e43322c64112de29251ef6b03bfb86c16f
kaviya2502 <kaviyarajakumarkaviyarajakumar@gmail.com>
2025-03-13 15:06:20 +0530

    leap year

---
+++ Leapyear/Leap.java
@@ -0,0 +1,13 @@
+public class Leap{
+    public static void main(String args[]){
+        int year=2020;
+        if(year%4==0){
+            System.out.println("Leap year");
+    
+        }
+        else{
+            System.out.println("Not a Leap year");
+        }
+        
+    }
+}
