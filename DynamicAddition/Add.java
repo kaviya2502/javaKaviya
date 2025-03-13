f2af43ad6c3838802eef78d41c66143fa60999de
kaviya2502 <kaviyarajakumarkaviyarajakumar@gmail.com>
2025-03-13 15:37:52 +0530

    dynamicaddition

---
+++ DynamicAddition/Add.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Add{
+    public static void main(String args[]){
+    Scanner input=new Scanner(System.in);
+    System.out.println("Enter num1:");
+    int number1=input.nextInt();
+    System.out.println("Enter num2:");
+    int number2=input.nextInt();
+    System.out.println("Addition of two numbers:");
+    
+    System.out.println(number1+number2);
+    }
+    }
