4d3ae358bf736cd54c4bec6d7c63919b509c355e
kaviya2502 <kaviyarajakumarkaviyarajakumar@gmail.com>
2025-03-13 11:11:21 +0530

    MajorMinorFinder

---
+++ MajorMinorFinder/MajorMinor.java
@@ -0,0 +1,11 @@
+public class MajorMinor{
+    public static void main(String args[]){
+        int age=25;
+        if(age>=18){
+            System.out.println("Major");
+        }
+        else{
+            System.out.println("minor");
+        }
+        }
+    }
