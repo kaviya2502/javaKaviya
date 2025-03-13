d24e3b55dfbebe1d065291d2aad060b19f938422
kaviya2502 <kaviyarajakumarkaviyarajakumar@gmail.com>
2025-03-13 10:56:44 +0530

    ConditionalStatus

---
+++ ConditionalStatement/Conditional.java
@@ -0,0 +1,10 @@
+public class Conditional{
+    public static void main(String args[]){
+        if(true){
+            System.out.println("Correct");
+        }
+    if(false){
+        System.out.println("Wrong");
+    }
+    }
+    }
