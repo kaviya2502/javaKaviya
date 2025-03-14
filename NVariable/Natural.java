ec565fabed3bd04c8010435f3471f2b7efc23408
kaviya2502 <kaviyarajakumarkaviyarajakumar@gmail.com>
2025-03-14 10:44:33 +0530

    natural

---
+++ NVariable/Natural.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Natural{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the numbers");
+        int num=input.nextInt();
+        int i=1;
+        while(i<=num){
+            System.out.println(i);
+            i=i+1;
+        }
+        }
+}
+    
