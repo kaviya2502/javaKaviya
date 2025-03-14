391c2b26658c65778a22d12f712264e816768733
kaviya2502 <kaviyarajakumarkaviyarajakumar@gmail.com>
2025-03-14 11:51:21 +0530

    Split

---
+++ Split/Split.java
@@ -0,0 +1,8 @@
+public class Split{
+    public static void main(String args[]){
+        String name="hello,world";
+        String names[]=name.split(",");
+        System.out.println(names[0]);
+        
+    }
+}
