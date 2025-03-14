2d48359eba284e2fd0afca80991f99e445745507
kaviya2502 <kaviyarajakumarkaviyarajakumar@gmail.com>
2025-03-14 15:51:30 +0530

    ffhfggy

---
+++ MultipleInputs/Multi.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Multi{
+    public static void main(String args[]){
+        Scanner input =new Scanner(System.in);
+        System.out.println("Enter the n value");
+        int n = input.nextInt();
+        int a[]= new int[n];
+        int i=0;
+        int j=0;
+        while(i<n){
+            System.out.println("Enter the value");
+            a[i]=input.nextInt();
+            i=i+1;
+        }
+        while(j<n){
+            System.out.println("Enter the value:"+(a[j]));
+            j=j+1;
+        }
+    }
+}
+        
