2f233948dde1f03de17fe1d18a15d3fbfe22e1ec
kaviya2502 <kaviyarajakumarkaviyarajakumar@gmail.com>
2025-03-14 10:07:58 +0530

    while

---
+++ While/Number.java
@@ -0,0 +1,11 @@
+
+public class Number{
+    public static void main(String args[]){
+        int i=92;
+        while(i<98){
+            System.out.println(i);
+            i=i+1;
+
+        }
+    }
+}
