57e699dd0fd781e1a45d1e51e08d19abbecc2918
kaviya2502 <kaviyarajakumarkaviyarajakumar@gmail.com>
2025-03-15 12:17:45 +0530

    elseif

---
+++ ElseIf/Else.java
@@ -0,0 +1,15 @@
+public class Else{
+    public static void main(String args[]){
+        int num=27;
+        if(num>0){
+            System.out.println("positive");
+        }
+        else if(num==0){
+            System.out.println("Neutral");
+        }
+        else{
+            System.out.println("negative");
+        }
+        }
+        }
+
