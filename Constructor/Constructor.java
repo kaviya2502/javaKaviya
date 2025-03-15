42da2dd4a0bbed4fb9e8079691db6ac300a3af84
kaviya2502 <kaviyarajakumarkaviyarajakumar@gmail.com>
2025-03-15 10:52:11 +0530

    constructor

---
+++ Constructor/Constructor.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Constructor{
+    Constructor(){
+        System.out.println("kaviya");
+    }
+    public static void main(String args[]){
+        Constructor obj= new Constructor();
+        obj.Constructor();
+
+    }
+
+    public void Constructor()
+    {
+        System.out.println("niki");
+    }
+    }
